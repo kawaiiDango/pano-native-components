364460c73af3aba47f3fd378bedb66adcfac60c1
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2025-09-14 22:56:19 +0530

    add aarch64

--- copy-builds-to-pano-dir.sh
+++ copy-builds-to-pano-dir.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cargo build --release
-cp target/release/libpano_native_components.so ../pano-scrobbler/composeApp/resources/linux-x64/;
-cp target/release/libnative_webview.so ../pano-scrobbler/composeApp/resources/linux-x64/;
+if [ "$(arch)" = "aarch64" ]; then
+    resourcesDirName="linux-arm64"
+else
+    resourcesDirName="linux-x64"
+fi
+cp target/release/libpano_native_components.so ../pano-scrobbler/composeApp/resources/$resourcesDirName/;
+cp target/release/libnative_webview.so ../pano-scrobbler/composeApp/resources/$resourcesDirName/;
